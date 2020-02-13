06d8f5e2352c97c889aac07fa93ca1d1346db492
jaheel <380565062@qq.com>
2020-02-13 19:14:21 +0800

    Dir to PUML

---
+++ DirToPUML.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+str="@startmindmap\n* begin\n"
+strend="@endmindmap"
+symbol="*"
+
+def get_file_path(root_path,symbol_level):
+    global str
+    #获取该目录下所有文件名称和目录名称
+    dir_or_files = os.listdir(root_path)
+    symbol=symbol_level+"*"
+    for dir_file in dir_or_files:
+        #获取目录或者文件的路径
+        dir_file_path=os.path.join(root_path,dir_file)
+        str+=symbol+" "+os.path.join(dir_file)+"\n"
+        if os.path.isdir(dir_file_path):
+            #递归获取所有文件和目录的路径
+            get_file_path(dir_file_path,symbol)
+        
+
+if __name__ == "__main__":
+    flag = True
+    while(flag):
+        rootpath =input("Pleanse input the dirctory(finish: end):")
+        if os.path.exists(rootpath):
+            print("Input Correct,the dirctory is reading,please wait")
+            get_file_path(rootpath,"*")
+            fileW=open("AutoRead.puml","w")  
+            fileW.write(str + strend)
+            print("Task is finished.")
+            fileW.close()
+        else:
+            if rootpath=="end":
+                flag =False
+            else:
+                print("Input Error,Reinput")
+        
+
+    
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# DirToPUML
-Directory auto read to PUML
+# How to use
+
+1. ```bash
+   py DirToPUML.py
+   ```
+
+2. 输入正确绝对地址（例如：F:\github\DirToPUML）
+
+3. 自动写入AutoRead.puml文件，可用PlantUML查看（VSCode插件实现）
